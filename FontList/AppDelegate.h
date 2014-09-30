64ee491c64be1ddac173bbcc2e17525a46d440c4
Andy <andrew.farrell@phgroup.com>
2014-09-30 20:22:37 +0100

    First commit.

--- FontList/AppDelegate.h
+++ FontList/AppDelegate.h
@@ -9,7 +9,21 @@
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+@property (weak) IBOutlet NSTableView *mainListView;
+@property (weak) IBOutlet NSTextField *previewTextField;
+@property (weak) IBOutlet NSTextField *fontSizeField;
+@property (weak) IBOutlet NSStepper *fontSizeStepper;
+@property (weak) IBOutlet NSTableCellView *listCell;
 
+- (IBAction)takePreviewTextFrom:(id)sender;
+- (IBAction)takeFontSizeFrom:(id)sender;
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)mainFontList;
+- (NSView *)tableView:(NSTableView *)mainFontList viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row;
 
 @end
 
+NSString *previewText;
+NSArray *fontFamilies;
+NSArray *filteredFontFamilies;
+NSUInteger fontSize;
--- FontList/AppDelegate.m
+++ FontList/AppDelegate.m
@@ -16,11 +16,79 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    
+    NSFontManager *manager = [NSFontManager sharedFontManager];
+    NSArray *unsortedSysFonts = [manager availableFontFamilies];
+    fontFamilies = [unsortedSysFonts sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+    unsortedSysFonts = nil;
+
+    previewText = @"The quick brown fox jumps over the lazy dog";
+    fontSize = [self.fontSizeField integerValue];
+    
+    [self.mainListView setDelegate:(id)self];
+    [self.mainListView setDataSource:(id)self];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+    fontFamilies = nil;
+}
+
+- (IBAction)takePreviewTextFrom:(id)sender {
+    NSString *newText = [sender stringValue];
+    if (newText != nil && [newText length] == 0) {
+        newText = [self.previewTextField.cell placeholderString];
+    }
+    previewText = newText;
+    
+    [self.mainListView reloadData];
+}
+
+- (IBAction)takeFontSizeFrom:(id)sender {
+    NSInteger newValue = [sender integerValue];
+    
+    if ([[sender identifier] isEqualToString:@"FontSizeField"]) {
+        self.fontSizeStepper.integerValue = newValue;
+    } else {
+        self.fontSizeField.integerValue = newValue;
+    }
+    
+    fontSize = newValue;
+    [self.mainListView reloadData];
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)mainFontList {
+    NSUInteger count = [fontFamilies count];
+    return count;
+}
+
+- (CGFloat)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row {
+    return lroundf(fontSize * 1.5);
+}
+
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+
+    NSTableCellView *cellView = [tableView makeViewWithIdentifier:tableColumn.identifier owner:self];
+    
+    NSString *contentText;
+    NSString *fontName = [fontFamilies objectAtIndex:row];
+    
+    if ([tableColumn.identifier isEqualToString:@"NameColumn"]) {
+        contentText = fontName;
+    } else {
+        cellView.textField.font = [NSFont fontWithName:fontName size:fontSize];
+        //cellView.textField.font
+
+        if (previewText == nil) {
+            contentText = [[self.previewTextField cell] placeholderString];
+        } else {
+            contentText = [previewText substringFromIndex:0];
+        }
+        
+    }
+    
+    cellView.textField.stringValue = contentText;
+    return cellView;
 }
 
 @end
